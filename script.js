5dc5e082a9961231dfe19c9f077675f9f52aff73
the-paranoid07 <121954633+the-paranoid07@users.noreply.github.com>
2023-07-03 20:50:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function volume_sphere() {
     //Write your code here
+	   const form = document.getElementById('MyForm');
+    const radius = form["radius"].value;
+    const volume = 4*Math.PI*radius*radius;
+    form["volume"].value = volume;
   
 } 
 
